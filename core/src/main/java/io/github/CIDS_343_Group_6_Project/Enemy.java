3262f7dcc649a6df610feb52950802b9a338f450
TimothyRF <timothy.furlong@my.uwrf.edu>
2025-04-07 08:44:50 -0500

    adding javadocs comments to player code

--- core/src/main/java/io/github/CIDS_343_Group_6_Project/Enemy.java
+++ core/src/main/java/io/github/CIDS_343_Group_6_Project/Enemy.java
@@ -1,7 +1,17 @@
 package io.github.CIDS_343_Group_6_Project;
 
+/**
+ * Enemy class under NPC
+ */
 public class Enemy extends NPC{
 
+    /**
+     * constructor for Enemy class
+     * @param enemy_name
+     * @param enemy_health
+     * @param enemy_magic
+     * @param enemy_intelligence
+     */
     public Enemy(String enemy_name, int enemy_health, int enemy_magic,int enemy_intelligence ){
         this.setControllable(false);
         this.setName(enemy_name);
--- core/src/main/java/io/github/CIDS_343_Group_6_Project/NPC.java
+++ core/src/main/java/io/github/CIDS_343_Group_6_Project/NPC.java
@@ -1,4 +1,8 @@
 package io.github.CIDS_343_Group_6_Project;
 
+/**
+ * Abstract class for a non-playable characters
+ * child of character class
+ */
 public class NPC extends Character{
 }
