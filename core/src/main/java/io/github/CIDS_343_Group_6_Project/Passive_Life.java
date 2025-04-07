f270253cf9a314ef86b1e9fc9f9c0143319a3965
TimothyRF <timothy.furlong@my.uwrf.edu>
2025-04-07 08:46:18 -0500

    adding javadocs comments to Passive_Life code

--- core/src/main/java/io/github/CIDS_343_Group_6_Project/Passive_Life.java
+++ core/src/main/java/io/github/CIDS_343_Group_6_Project/Passive_Life.java
@@ -1,7 +1,18 @@
 package io.github.CIDS_343_Group_6_Project;
 
+/**
+ * passive characters class under NPC
+ * may be traders, pedestrians, etc
+ */
 public class Passive_Life extends NPC{
 
+    /**
+     * Constructor for Passive_Life class
+     * @param life_name
+     * @param life_health
+     * @param life_magic
+     * @param life_intelligence
+     */
     public Passive_Life(String life_name, int life_health, int life_magic,int life_intelligence ){
         this.setControllable(false);
         this.setName(life_name);
