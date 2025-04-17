9936e691cf52cd121e5674f07f9cc948a9bb518f
JeremyWanguhu <jeremy.wanguhu@my.uwrf.edu>
2025-04-17 12:49:33 -0500

    HEllO I made a new branch

--- core/src/main/java/io/github/CIDS_343_Group_6_Project/Driver.java
+++ core/src/main/java/io/github/CIDS_343_Group_6_Project/Driver.java
@@ -2,51 +2,38 @@ package io.github.CIDS_343_Group_6_Project;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.Array;
 
-// Adding Comment for Preliminary Project Research Assignment
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Driver implements ApplicationListener {
-    Texture backGroundTexture;
-    Texture bucketTexture;
-    Texture dropTexture;
-    Sound dropSound;
-    Music music;
-    SpriteBatch spriteBatch;
-    FitViewport viewport;
-    Sprite bucketSprite;
-    Vector2 touchPos;
-    Array<Sprite> dropSprites;
-    float dropTimer;
-    Rectangle bucketRectangle;
-    Rectangle dropRectangle;
+    private SpriteBatch spriteBatch;
+    private OrthographicCamera camera;
+    private FitViewport viewport;
+
+    private Texture bucketTexture;
+    private Sprite bucketSprite;
+    private Vector2 touchPos;
+    private Array<Sprite> dropSprites;
+    private Rectangle bucketRectangle;
+    private Rectangle dropRectangle;
 
     @Override
     public void create() {
-        backGroundTexture = new Texture("background.png");
-        bucketTexture = new Texture("bucket.png");
-        dropTexture = new Texture("drop.png");
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(8, 5, camera); // World units
         spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+
+        bucketTexture = new Texture("bucket.png");
         bucketSprite = new Sprite(bucketTexture);
-        bucketSprite.setSize(1, 1);
+        bucketSprite.setSize(1, 1); // 1x1 world unit
+
         touchPos = new Vector2();
         dropSprites = new Array<>();
         bucketRectangle = new Rectangle();
@@ -60,106 +47,42 @@ public class Driver implements ApplicationListener {
 
     @Override
     public void render() {
-        // Hello World!!!
         input();
         logic();
         draw();
     }
 
     private void input() {
-        float speed = 4f;
-        float delta = Gdx.graphics.getDeltaTime();
-
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            bucketSprite.translateX(speed * delta);
-        } else if (Gdx.input.isKeyPressed((Input.Keys.LEFT))) {
-            bucketSprite.translateX((-speed * delta));
-        }
-
-        if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            bucketSprite.setCenterX(touchPos.x);
-        }
+// Your original input handling here (e.g., touch to move bucket)
     }
 
     private void logic() {
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-        float bucketWidth = bucketSprite.getWidth();
-        float bucketHeight = bucketSprite.getHeight();
-
-        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth- bucketWidth));
-
-        float delta = Gdx.graphics.getDeltaTime();
-        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
-
-        for (int i = dropSprites.size - 1; i >= 0; i--) {
-            Sprite dropSprite = dropSprites.get(i);
-            float dropWidth = dropSprite.getWidth();
-            float dropHeight = dropSprite.getHeight();
-
-            dropSprite.translateY(-2f * delta);
-            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
-
-            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
-            else if (bucketRectangle.overlaps(dropRectangle)) {
-                dropSprites.removeIndex(i);
-            }
-        }
-
-        dropTimer += delta;
-        if (dropTimer > 1f) {
-            dropTimer = 0;
-            createDroplet();
-        }
+// Your original game logic here (e.g., spawn and move raindrops)
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.BLACK);
-        viewport.apply();
-        spriteBatch.setProjectionMatrix((viewport.getCamera().combined));
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+
         spriteBatch.begin();
-
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        spriteBatch.draw(backGroundTexture, 0, 0, worldWidth, worldHeight);
         bucketSprite.draw(spriteBatch);
-
-        for(Sprite dropSprite : dropSprites) {
-            dropSprite.draw(spriteBatch);
-        }
-
+// draw raindrops, etc. here
         spriteBatch.end();
     }
 
-    private void createDroplet() {
-        float dropWidth = 1;
-        float dropHeight = 1;
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        Sprite dropSprite = new Sprite(dropTexture);
-        dropSprite.setSize(dropWidth, dropHeight);
-        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
-        dropSprite.setY(worldHeight);
-        dropSprites.add(dropSprite);
-    }
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        spriteBatch.dispose();
+        bucketTexture.dispose();
+// Dispose any other textures if needed
     }
 }
-
-//Change//
