8801ce17a51ba7cc14bb9811eb9c5372c95d6525
314159rate <ethanreichel02@gmail.com>
2025-04-17 12:44:39 -0500

    Made MapGenerator fully testable

--- core/src/main/java/map/main.java
+++ core/src/main/java/map/main.java
@@ -6,26 +6,37 @@ import map.Chunk;
 public class main {
     public static void main(String[] args) {
         MapGenerator MapG = new MapGenerator();
+        int size = 9; //dimensions of the map as a whole
+        int numRooms = 50; //how many chunks will be generated
+        float spread = .25f; //roughly how spread out the chunks will be (0 - 1)
+        int chunksize = 9; //dimensions of each chunk
+        float obstacleDensity = .25f; //roughly how many obstacles will be in each chunk (0 - 1)
         ChunkGenerator ChunkG = new ChunkGenerator();
         System.out.println("Hello World");
-        Chunk[][] newMap = MapG.generateMap(5,12,1, 5, .5f);
+        Chunk[][] newMap = MapG.generateMap(size,numRooms,spread, chunksize, obstacleDensity);
         //int[][] newChunk = ChunkG.GenerateChunk(5,true,true,false,false, .5f);
-        for(int i = 0; i < 5; i++) { //i,j is map ::: k,l is chunk
-            for (int k = 0; k < 5; k++){
-                for(int j = 0; j < 5; j++){
-                    for(int l = 0; l < 5; l++){
-                        if(newMap[i][j].getActive()) {
-                            System.out.print(newMap[i][j].getChunk()[k][l] + " ");
+        for(int x = 0; x < size; x++) { //i,j is map ::: k,l is chunk
+            for (int x1 = 0; x1 < chunksize; x1++){
+                for(int y = 0; y < size; y++){
+                    for(int y1 = 0; y1 < chunksize; y1++){
+                        if(newMap[x][y].getActive() && newMap[x][y].getChunk()[x1][y1] != 0) {
+                            System.out.print(newMap[x][y].getChunk()[x1][y1] + " ");
                         }else{
                             System.out.print("- ");
                         }
                     }
-                    System.out.print("|");
+                    System.out.print("| ");
                 }
                 System.out.println();
 
             }
-            System.out.println("----------+----------+----------+----------+----------");
+            for(int y = 0; y < size; y++){
+                for(int y1 = 0; y1 < chunksize; y1++){
+                    System.out.print("--");
+                }
+                System.out.print("+-");
+            }
+            System.out.println();
         }
     }
 }
