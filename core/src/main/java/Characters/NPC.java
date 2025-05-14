b053af79e41a94a76e1d4fac872bd99f59068274
PatrickSwede <pswede07@hotmail.com>
2025-05-14 01:20:14 -0500

    Fixing merge conflicts

--- core/src/main/java/Characters/NPC.java
+++ core/src/main/java/Characters/NPC.java
@@ -3,7 +3,6 @@ package Characters;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-
 /**
  * Abstract class for a non-playable characters
  * child of character class
@@ -13,5 +12,4 @@ public class NPC extends Character {
     NPC(Vector2 pos, TextureRegion texture, float width, float height, int health) {
         super(new Vector2(pos.x, pos.y), texture, width, height, health);
     }
-
 }
--- core/src/main/java/Characters/Player.java
+++ core/src/main/java/Characters/Player.java
@@ -24,8 +24,8 @@ public class Player extends Character {
      * @param player_name
      * set Controllable from character parent to true
      */
-    public Player(Vector2 pos, TextureRegion texture, float width, float height, String player_name, int health){
-        super(new Vector2(pos.x, pos.y), texture, width, height, health);
+    public Player(Vector2 pos, TextureRegion texture, float width, float height, String player_name, int playerHealth){
+        super(new Vector2(pos.x, pos.y), texture, width, height, playerHealth);
         this.name = player_name;
         this.setControllable(true);
     }
